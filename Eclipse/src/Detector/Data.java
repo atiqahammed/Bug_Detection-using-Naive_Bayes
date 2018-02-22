40efc1269afec4c8aeca778f86a1e698568329aa
atiqahammed <atiqahammedshamim@gmail.com>
2018-02-22 16:53:08 +0600

    about done

--- Eclipse/src/Detector/Data.java
+++ Eclipse/src/Detector/Data.java
@@ -2,20 +2,26 @@ package Detector;
 
 public class Data {
 	private String[] valueOfAttributes;
-	
+
 	public Data(String[] valueOfAttributes) {
 		this.valueOfAttributes = valueOfAttributes;
 	}
-	
-	public Data() {
-	}
-	
+
+
+
 	public String getValueInIndex(int index) {
 		return valueOfAttributes[index];
 	}
-	
+
 	public int getColumnSize(){
 		return valueOfAttributes.length;
 	}
-	
+
+
+
+	public String[] getAllDataValue() {
+		// TODO Auto-generated method stub
+		return valueOfAttributes;
+	}
+
 }
